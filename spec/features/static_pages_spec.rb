16dc9a83b272698ac764de8ce4d6e8a586825a7b
Caleb Kobzeff <kobzeffc@gmail.com>
2014-02-13 15:51:22 -0800

    Remove Home page link from navbar

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,10 +5,9 @@ shared_examples_for "all pages" do |page_title|
 end
 
 shared_examples_for "pages with navbar" do
-  it { should have_selector('nav') }
-  it { should have_link("About") }
-  it { should have_link("Home") }
+  it { should have_selector('nav.top-bar') }
   it { should have_link("My App") }
+  it { should have_link("About") }
 end
 
 feature "static_pages" do
@@ -19,13 +18,13 @@ feature "static_pages" do
       visit root_path
       click_link "About"
       expect(page).to have_title(full_title("About"))
-      click_link "Home"
+      click_link "My App"
       expect(page).to have_title(full_title(""))
     end
   end
 
   feature "Home page" do
-    before { visit root_path } 
+    before { visit root_path }
 
     it_behaves_like "all pages", ""
     it_behaves_like "pages with navbar"
