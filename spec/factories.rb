95f6a8a1a3ece646dc28937f6fff8859060511c0
Caleb Kobzeff <kobzeffc@gmail.com>
2014-02-19 19:10:56 -0800

    Finish user signup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name                    "Caleb"
-    email                   "caleb@example.com"
-    password                "foobar"
-    password_confirmation   "foobar"
+    name      'Caleb'
+    email     'caleb@example.com'
+    password  'foobar'
+    password_confirmation  'foobar'
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
+def fill_in_signup
+  within('#signup') do
+    fill_in 'Name', with: "exampleuser"
+    fill_in 'Email', with: "exampleuser@example.com"
+    fill_in 'Password', with: "foobar"
+    fill_in 'Confirm Password', with: "foobar"
+  end
+end
+
 feature "user actions" do
 
   subject { page }
@@ -9,34 +18,45 @@ feature "user actions" do
 
   feature "signing up" do
 
-    before { visit signup_path }
+    before :each do
+      visit signup_path
+    end
+
     it_behaves_like "all pages", "Sign up"
     it_behaves_like "pages with navbar"
 
     scenario "with valid information" do
-      visit signup_path
-
-      within('#signup') do
-        fill_in 'Name', with: user.name
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: user.password
-        fill_in 'Confirm Password', with: 'foobar'
-      end
+      fill_in_signup
 
       expect { click_button submit }.to change(User, :count).by(1)
     end
 
+    scenario "after saving the user" do
+      fill_in_signup
+
+      click_button submit
+      expect(page).to have_title("exampleuser")
+      expect(page).to have_selector('div', text: 'Welcome')
+    end
+
     scenario "with no information" do
-      visit signup_path
       expect { click_button submit }.not_to change(User, :count)
     end
+
+    scenario "with invalid information" do
+      click_button submit
+
+      expect(page).to have_title 'Sign up'
+      expect(page).to have_content 'error'
+    end
+
   end
 
   feature "visiting profile page" do
     before { visit user_path(user) }
 
     it_behaves_like "pages with navbar"
-    it { should have_title(user.name) }
-    it { should have_content(user.email) }
+    it { should have_title user.name }
+    it { should have_content user.email }
   end
 end
